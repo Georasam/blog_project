eaf577c002b4461d1128173cffa1f5af590b4a80
Georasam <asamoahg001@gmail.com>
2022-08-13 16:01:37 +0200

    reactbootstrap

--- src/Components/html.js
+++ src/Components/html.js
@@ -1,31 +1,6 @@
-import React from 'react'
-import {useEffect, useState} from 'react'
-function GetData() {
-  const [entries, setEntries] = useState(false)
+import React from 'react';
 
-  useEffect(()=> {
-    loadEntries()
-  },[])
-  async function loadEntries(){
-    const url = `https://cdn.contentful.com/spaces/${process.env.REACT_APP_SPACE_ID}/environments/${process.env.REACT_APP_ENVIRONMENT}/entries?access_token=${process.env.REACT_APP_ACCESS_TOKEN}`
-    const response =await fetch(url)
-    const result= await response.json()
-   
-    setEntries(result.items)
-
-  }
- 
- if(!entries){
-    return <div>Data is Loading...</div>
-  }
-  
-  const posts = entries.filter((element)=> element.sys.contentType.sys.id.includes("game") ) 
-  console.log(posts) 
-  return (
-    <div>
-      <h1>DATA FETCHED</h1>
-
-      <div class="row">
+<div class="row">
   <div class="col-sm-6">
     <div class="card">
       <div class="card-body">
@@ -59,9 +34,3 @@ function GetData() {
 </div>
 
     
-      
-    </div>
-  )
-}
-
-export default GetData
