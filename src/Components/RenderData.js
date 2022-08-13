91b00b2be657e35c2f3885f5b7536922142d1be6
a-lavi <ali.rahmani.63@icloud.com>
2022-08-13 16:43:21 +0200

    add second component renderData

--- src/Components/RenderData.js
+++ src/Components/RenderData.js
@@ -1,8 +1,29 @@
 import React from 'react'
-
-function RenderData() {
+import { Button } from 'react-bootstrap';
+function RenderData({entries}) {
+    console.log(entries )
   return (
-    <div>RenderData</div>
+    
+        
+      <div className="row">
+       <div>{ entries.map((entry,index)=>{
+return(
+<div key={index} className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">{entry.fields.title}</h5>
+         <img src=''/>
+        <p className="card-text">HUNTER OF THE NIGHT</p>
+        <Button href="#" variant='success' className="btn btn-primary">watch movie</Button>
+      </div>
+    </div>
+  </div>
+)
+}) }
+  
+  </div> 
+</div>
+    
   )
 }
 
