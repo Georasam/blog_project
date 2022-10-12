090f9fdbee00eec6f72444d95a07296b051c9638
a-lavi <ali.rahmani.63@icloud.com>
2022-10-12 23:26:14 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -47,11 +47,7 @@ function App() {
   const { auth } = useAuth();
   const navigate = useNavigate();
 
-  //REGISTER USERNAME FUNCTION
-  /*  async function registerUsername(element) {
-    const games = await registerControl(element);
-    setData(games);
-  } */
+  
 
   //CHANGE PASSWORD (NOT)
   async function changePassElement(element) {
@@ -59,10 +55,7 @@ function App() {
     setData(games);
   }
 
-  /* async function loginUsername(element) {
-    const games = await loginControl(element);
-    setData(games);
-  } */
+  
 
   async function getGames() {
     const games = await getBoardGames();
@@ -72,11 +65,6 @@ function App() {
       return { ...prev, games };
     }); */
   }
-
-  /*  async function getSingleGame(){
-    const singleGame = await getSingleBoardGame();
-    setSingleGame(singleGame)
-  } */
 
   async function updateGame(element) {
     const games = await gameEdit(element);
@@ -95,20 +83,7 @@ function App() {
     setData(games);
   }
 
-  /*   async function getGames() {
-    try {
-      setLoadingA(true);
-      const games = await getBoardGames();
-      setData(games);
-    } catch (e) {
-      setErrorA(e);
-    } finally {
-      setLoadingA(false);
-    }
-    if (errorA) return "Failed to load resource A";
-  return loadingA ? "Loading..." : games;
-  }
-   */
+ 
 
   useEffect(() => {
     getGames();
@@ -117,18 +92,7 @@ function App() {
   const logout = useLogout();
   console.log(logout); 
 
-  async function search() {
-    const url = `https://cdn.contentful.com//spaces/${process.env.REACT_APP_SPACE_ID}/environments/${process.env.REACT_APP_ENVIRONMENT}/entries?access_token=${process.env.REACT_APP_ACCESS_TOKEN}&query=${searchQuery}`;
-    const response = await fetch(url);
-    const result = await response.json();
-    console.log(result);
-    setSearchResults(result.items); //rich test
-  }
-
-  /* const onClickHome = () => {
-    const homeUrl = "http://localhost:3000/blog_project";
-    window.location = homeUrl;
-  }; */
+ 
 
   const signOut = async () => {
     await logout();
@@ -166,7 +130,7 @@ function App() {
       <NavScroll
         searchQuery={searchQuery}
         setSearchQuery={setSearchQuery}
-        search={search}
+       
       />
 
       <Routes>
@@ -187,14 +151,9 @@ function App() {
           element={<GameDetails />}
         />
 
-        
-          <Route
-            exact
-            path="/blog_project"
-            element={<RenderData data={data} />}
-          />
+          
 
-          <Route path="/blog_project/test" element={<Album data={data} />} />
+         
 
           <Route
             path="/blog_project/login"
@@ -206,7 +165,7 @@ function App() {
             path="/blog_project/search"
             element={
               <Search
-                search={search}
+                
                 searchQuery={searchQuery}
                 searchResults={searchResults}
               />
@@ -220,9 +179,10 @@ function App() {
           />
 
           <Route element={<PersistLogin />}>
-            <Route
-              element={<RequireAuth allowedRoles={[ROLES.Admin, ROLES.User]} />}
-            >
+
+
+            <Route element={<RequireAuth allowedRoles={[ROLES.Admin, ROLES.User]} />}>
+              
               <Route
                 exact
                 path="/blog_project/register"
@@ -253,12 +213,7 @@ function App() {
                 exact
                 path="/blog_project/change-password"
                 element={
-                  <ChangePass
-                    search={search}
-                    data={data}
-                    searchQuery={searchQuery}
-                    searchResults={searchResults}
-                  />
+                  <ChangePass />
                 }
               />
             </Route>
@@ -271,10 +226,7 @@ function App() {
           element={<ContactForm />}
         />
         <Route exact path="/blog_project/aboutus" element={<AboutUs />} />
-        {/*  <Route
-            <ChangePass data={data} changePassElement={changePassElement} />
-          
-        /> */}
+        
 
         <Route path="*" element={<Missing />} />
       </Routes>
